1b5f93b2f0fb4647a6c0c0115d3116dadac7d757
Tekad <alessandroconnell@gmail.com>
2024-02-02 09:06:46 +0100

    added cube

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import loadPrcFile
+from panda3d.core import loadPrcFile, Vec3
+from panda3d.bullet import BulletWorld, BulletDebugNode
+from physics import physics
 
 class window(ShowBase):
     def __init__(self, fStartDirect=True, windowType=None):
+        loadPrcFile("config/config.prc")
         super().__init__(fStartDirect, windowType)
+
+        self.bullet_world = BulletWorld()
+        self.bullet_world.setGravity(Vec3(0, 0, -9.81))
+
+        self.NewObject(scale=(10,10,1))
+    
+    def NewObject(self, model='models/cube.egg', position=(0,0,0), scale=(1,1,1), texture='textures/blue.png'):
+        self.object = self.loader.loadModel(model)
+        self.object.setPos(position)
+        self.object.setScale(scale)
+        texture = self.loader.loadTexture(texture)
+        self.object.setTexture(texture)
+        physics.applyBoxCollider(self, self.object)
+        self.object.reparentTo(self.render)
+    
+
+    def update(self, task):
+        dt = self.globalclock.dt
+
+        key_down = self.mouseWatcherNode.isButtonDown
+
+        self.bullet_world.doPhysics(dt)
+
+
+        if key_down("escape"):
+            quit()
+
+        return task.cont
 
 window().run()
---
+++ physics.py
@@ -0,0 +1,28 @@
+from panda3d.bullet import BulletBoxShape, BulletRigidBodyNode, BulletPlaneShape
+
+class physics():
+    def applyBoxCollider(self, node_path):
+        col_shape = BulletBoxShape((0.5, 0.5, 0.5))
+
+        col_node = BulletRigidBodyNode('box_collider')
+        col_node.addShape(col_shape)
+
+        col_np = self.render.attachNewNode(col_node)
+        col_np.reparentTo(node_path)
+                
+        self.bullet_world.attachRigidBody(col_node)
+
+        return col_np  
+    
+    def applyPlaneCollider(base, node_path, normal = (0.0, 0.0, 1.0), dist = 0.0):
+        col_shape = BulletPlaneShape(normal, dist)
+
+        col_node = BulletRigidBodyNode("plane_collider")
+        col_node.addShape(col_shape)
+
+        col_np = base.render.attachNewNode(col_node)
+        col_np.reparentTo(node_path)
+                
+        base.bullet_world.attachRigidBody(col_node)
+
+        return col_np
